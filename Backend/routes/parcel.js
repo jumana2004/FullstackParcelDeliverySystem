ee4b9e1c6c46e8529710692c7ff78c56efbb127d
jumana <fathimathjumana2004@gmail.com>
2024-11-15 13:54:53 +0530

    created authentication

---
+++ Backend/routes/parcel.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const {createParcel, getAllParcels, updateParcel, getOneParcel, getUserParcel, deleteParcel} = require("../controllers/parcel");
+// const { verifyToken, verifyTokenAndAuthorization } = require('../middlewares/verifyToken');
+
+
+
+//ADD PARCEL 
+router.post("/", createParcel)
+
+
+//GET ALL PARCEL
+router.get("/", getAllParcels)
+
+
+// UPDATE PARCEL
+router.put ("/:id", updateParcel)
+
+
+// GET ONE PARCEL
+
+router.get("/find/:id" ,getOneParcel)
+
+// GET USERS PARCELS
+
+router.post("/me", getUserParcel)
+
+// DELETE PARCEL
+
+router.delete("/:id", deleteParcel)
+
+
+
+
+module.exports = router;
